2a4af4d8c40eafbe26015922d52191c182d08e3f
Jaeyeon Kim <0310kjy@gmail.com>
2022-12-14 23:26:57 +0900

    fix : error solve

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 // import "./index.css";
 // import reportWebVitals from "./reportWebVitals";
 import AppRouter from "./AppRouter";
@@ -7,8 +10,10 @@ import AppRouter from "./AppRouter";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <App />
     <AppRouter />
   </React.StrictMode>
 );
 
+reportWebVitals();
 // reportWebVitals();
