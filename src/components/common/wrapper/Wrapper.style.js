e5ac5dbb8e03aa5db1bcbed4ebfa83514efc3d6b
Jaeyeon Kim <0310kjy@gmail.com>
2022-12-15 05:57:07 +0900

    feat : add local address page

--- src/components/common/wrapper/Wrapper.style.js
+++ src/components/common/wrapper/Wrapper.style.js
@@ -2,13 +2,13 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   width: 39rem;
   height: 84.4rem;
-  background-color: red;
   left: 0;
   right: 0;
   margin: auto;
 `;
 const WrapperInner = styled.div`
   top: 4.7rem;
+  position:relative;
 `;
 
 export { Wrapper, WrapperInner };
