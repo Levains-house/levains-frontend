96d23d959957a461fda93d082aa94fcd7993e442
Jaeyeon Kim <0310kjy@gmail.com>
2022-12-16 05:32:09 +0900

    feat : home refactoring

--- src/pages/home/ItemInfo.js
+++ src/pages/home/ItemInfo.js
@@ -14,22 +14,22 @@ const ItemInfo = (props) =>{
         <S.grayWindow></S.grayWindow>
         <S.whiteBack></S.whiteBack>
         <S.exitButton onClick={()=>props.setModalNum(-1)}>X</S.exitButton>
-        <S.titleText>르방 상세설명</S.titleText>
+        <S.titleText>주멍 상세설명</S.titleText>
         <S.itemContainer></S.itemContainer>
         <S.itemimg></S.itemimg>
         <S.dot1></S.dot1>
-        <S.text1>교환 희망 르방</S.text1>
+        <S.text1>내가 원하는 상품</S.text1>
         <S.categoryBox>{props.category}</S.categoryBox>
         <S.line1></S.line1>
         <S.nameText>{props.name}</S.nameText>
         <S.descriptionText>{props.description}</S.descriptionText>
         <S.wantBox></S.wantBox>
         <S.dot2></S.dot2>
-        <S.text2>상대방 희망 르방</S.text2>
-        <S.categoryBox2>{props.category}</S.categoryBox2>
+        <S.text2>상대방이 원하는 상품</S.text2>
+        <S.categoryBox2>{props.want_category}</S.categoryBox2>
         <S.line2></S.line2>
-        <S.text3>이름</S.text3>
-        <S.text4>내용</S.text4>
+        <S.text3>{props.want_name}</S.text3>
+        <S.text4>{props.want_category}</S.text4>
         <S.zzim><S.zzimText>찜하기</S.zzimText></S.zzim>
         <a href={props.kakao_talk_chatting_url}><S.chatButton>채팅하기</S.chatButton></a>
         </>
