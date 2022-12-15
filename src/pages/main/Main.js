59ddb5a2953cd85c7a78683731f5a5239df52ecf
Jaeyeon Kim <0310kjy@gmail.com>
2022-12-15 15:50:32 +0900

    feat : add main header & nav

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,8 +1,38 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Wrapper from "../../components/common/wrapper/Wrapper";
+import * as S from "./Main.style"
+import headerImage from "../../assets/images/여행객/Vector.svg"
+import homeImage from "../../assets/images/homeicon.png"
+import levainImage from "../../assets/images/levainicon.png"
+import Home from "../home/Home"
+import Mypage from "../myPage/Mypage"
+import logoImage from "../../assets/images/logo.svg"
 const Main = () => {
+    const [isHome, Toggle] = useState(true);
     return(
-        <span>Main</span>
+        <Wrapper>
+            <S.headerWhiteBox></S.headerWhiteBox>
+            <S.headerButton><S.headerImg src={headerImage}></S.headerImg></S.headerButton>
+            {isHome ? 
+            <>
+            <S.headerText>혜연님,</S.headerText>
+            <S.headerText2>제주에서&nbsp;<text style={{color:'#86C3C7'}}>특별한 만남을</text>&nbsp;나눠보세요</S.headerText2>
+            <Home/>
+            </>
+            :
+            <>
+            <S.headerText>혜연님,</S.headerText>
+            <S.headerText2><text style={{color:'#86C3C7'}}>나의 르방</text>&nbsp;확인해보세요!</S.headerText2>
+            <Mypage />
+            </>
+            }
+            <S.bottomNavContainer></S.bottomNavContainer>
+            <S.logoButton><S.logoImg src={logoImage}></S.logoImg></S.logoButton>
+            <S.homeButton onClick={()=>{Toggle(true)}}><S.homeImg src={homeImage}></S.homeImg></S.homeButton>
+            <S.homeText>홈</S.homeText>
+            <S.levainButton onClick={()=>{Toggle(false)}}><S.levainImg src={levainImage}></S.levainImg></S.levainButton>
+            <S.levainText>나의 르방</S.levainText>
+        </Wrapper>
     )
 }
 
---
+++ src/pages/main/Main.style.js
@@ -0,0 +1,163 @@
+import styled from "styled-components";
+
+export const headerWhiteBox = styled.div`
+    position: absolute;
+    top: 0;
+    width: 39rem;
+    height: 20rem;
+    background-color: white;
+`
+export const headerButton = styled.button`
+    position: absolute;
+    width: 1.6rem;
+    height:0.941rem;
+    top: 4.7rem;
+    left:1.7rem;
+    background:white;
+    border: 5px;
+`
+export const headerText = styled.text`
+position: absolute;
+left: 1.9rem;
+top: 10rem;
+
+/* Mediam/sub M 20px */
+
+font-family: 'Noto Sans KR';
+font-style: normal;
+font-weight: 500;
+font-size: 2rem;
+line-height: 2.9rem;
+/* identical to box height */
+
+display: flex;
+align-items: center;
+
+/* gray scale/gray4 */
+
+color: #3E404C;
+`
+export const headerText2 = styled.text` 
+position: absolute;
+left: 1.9rem;
+top: 12.9rem;
+
+/* Mediam/sub M 20px */
+
+font-family: 'Noto Sans KR';
+font-style: normal;
+font-weight: 500;
+font-size: 2rem;
+line-height: 2.9rem;
+/* identical to box height */
+
+display: flex;
+align-items: center;
+
+/* gray scale/gray4 */
+
+color: #3E404C;
+`
+
+export const headerImg = styled.img`
+    height: 1.481rem;
+    width: 1.534rem;
+`
+
+export const bottomNavContainer = styled.div`
+    position: absolute;
+    width: 39rem;
+    height: 7.7rem;
+    bottom: 0;
+
+    background: #FFFFFF;
+    box-shadow: 0px -5px 8px rgba(0, 0, 0, 0.05);
+    border-radius: 20px 20px 0px 0px;
+`
+
+export const homeButton = styled.button`
+    position: absolute;
+    width: 2.267rem;
+    height:2.54rem;
+    bottom: 3.46rem;
+    left:7.9rem;
+    background:white;
+    border: 5px;
+`
+export const homeImg = styled.img`
+    height: 2.54rem;
+    width: 2.267rem;
+`
+
+export const homeText = styled.text`
+    position: absolute;
+    left: 6.7rem;
+    bottom: 1.56rem;
+
+    font-family: 'Noto Sans';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1.2rem;
+    line-height: 1.6rem;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    letter-spacing: -0.028rem;
+
+    /* main color */
+
+    color: #FFD058;
+    height: 1.6rem;
+    width: 4.7rem;
+    transform:translate(45%, 0);
+`
+
+export const levainButton = styled.button`
+    position: absolute;
+    width: 2.267rem;
+    height:2.54rem;
+    bottom: 3.9rem;
+    right:7.9rem;
+    background:white;
+    border: 5px;
+`
+export const levainImg = styled.img`
+    height: 2.54rem;
+    width: 2.267rem;
+`
+
+export const levainText = styled.text`
+    position: absolute;
+    right: 6.0rem;
+    bottom: 1.7rem;
+
+    font-family: 'Noto Sans';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1.2rem;
+    line-height: 1.6rem;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    letter-spacing: -0.028rem;
+    transform:translate(20%, 0);
+    /* main color */
+
+    color: #FFD058;
+    height: 1.6rem;
+    width: 6.0rem;
+`
+
+export const logoButton = styled.button`
+    position: absolute;
+    width: 6.8rem;
+    height:6.8rem;
+    bottom: 3.4rem;
+    left:16.2rem;
+    background:white;
+    border: 5px;
+`
+export const logoImg = styled.img`
+    height: 6.8rem;
+    width: 6.8rem;
+`
