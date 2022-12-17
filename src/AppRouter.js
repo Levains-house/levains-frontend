7a4d9fb7e974a8b70e95338267822a2d4ecf809c
Jaeyeon Kim <0310kjy@gmail.com>
2022-12-17 22:15:57 +0900

    Error fix

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LocalAddress from "./pages/local/localAddress/LocalAddress";
 import LocalItems from "./pages/local/localItems/LocalItems";
@@ -10,7 +10,15 @@ import TravelItems from "./pages/travel/travelItems/TravelItems";
 import { RecoilRoot } from "recoil";
 import Items from "./pages/items/Items";
 
+
 const AppRouter = () => {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
   return (
     <div className="Approuter">
       <RecoilRoot>
